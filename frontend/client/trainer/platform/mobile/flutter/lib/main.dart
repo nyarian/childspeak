5037871942b6e4bbff6873b2d94eccb7eb40159d
Andrey Ilyunin <orkay255@gmail.com>
2020-06-25 00:21:56 +0300

    Refactor analysis_options.yaml and main.dart

--- frontend/client/trainer/platform/mobile/flutter/lib/main.dart
+++ frontend/client/trainer/platform/mobile/flutter/lib/main.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-void main() => runApp(ChildSpeak());
+void main() => runApp(const ChildSpeak());
 
 class ChildSpeak extends StatelessWidget {
+  const ChildSpeak({Key key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Text('ChildSpeak'),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: "Flutter Demo",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const Text("ChildSpeak"),
+      );
 }
