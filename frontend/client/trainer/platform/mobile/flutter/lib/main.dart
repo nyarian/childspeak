d68368d5f92488c01107590c75bd9a0efa633c37
Andrey Ilyunin <orkay255@gmail.com>
2020-06-28 04:46:04 +0300

    Configure MaterialApp with basic app colors

--- frontend/client/trainer/platform/mobile/flutter/lib/main.dart
+++ frontend/client/trainer/platform/mobile/flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:childspeak/i18n/intl_delegate.dart';
+import 'package:childspeak/ui/page/speaking_session.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -9,17 +10,21 @@ class ChildSpeak extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
+        title: 'ChildSpeak',
         localizationsDelegates: <LocalizationsDelegate<dynamic>>[
           const IntlDelegate(),
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
           GlobalCupertinoLocalizations.delegate,
         ],
+        supportedLocales: IntlDelegate.supportedLocales,
+        color: Colors.blue,
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          primaryColor: Colors.blue,
+          accentColor: Colors.yellowAccent,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: const Text('ChildSpeak'),
+        home: const SpeakingSessionPage(),
       );
 }
