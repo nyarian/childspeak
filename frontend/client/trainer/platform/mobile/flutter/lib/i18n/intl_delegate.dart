f4d79744b30e6d26aab618bc35f3a9f4a6416498
Andrey Ilyunin <orkay255@gmail.com>
2020-06-28 04:30:09 +0300

    Setup intl-backed i18n

---
+++ frontend/client/trainer/platform/mobile/flutter/lib/i18n/intl_delegate.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:childspeak/i18n/generated/messages_all.dart';
+
+class IntlDelegate implements LocalizationsDelegate<bool> {
+  static const List<Locale> supportedLocales = <Locale>[
+    Locale('en'),
+    Locale('ar')
+  ];
+
+  const IntlDelegate();
+
+  @override
+  bool isSupported(Locale locale) => supportedLocales.contains(locale);
+
+  @override
+  Future<bool> load(Locale locale) async {
+    Intl.defaultLocale = locale.toString();
+    return initializeMessages(locale.toString());
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<dynamic> old) => true;
+
+  @override
+  Type get type => bool;
+}
--- frontend/client/trainer/platform/mobile/flutter/lib/main.dart
+++ frontend/client/trainer/platform/mobile/flutter/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:childspeak/i18n/intl_delegate.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(const ChildSpeak());
 
@@ -8,6 +10,12 @@ class ChildSpeak extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: 'Flutter Demo',
+        localizationsDelegates: <LocalizationsDelegate<dynamic>>[
+          const IntlDelegate(),
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
