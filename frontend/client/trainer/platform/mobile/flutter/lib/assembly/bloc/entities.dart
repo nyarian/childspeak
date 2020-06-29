1c89a2b7758d3756a1fbbd25b9ac8cc0c23a0f3d
Andrey Ilyunin <orkay255@gmail.com>
2020-06-29 17:37:07 +0300

    Nuke flutter_gateway module

--- frontend/client/trainer/platform/mobile/flutter/lib/assembly/bloc/entities.dart
+++ frontend/client/trainer/platform/mobile/flutter/lib/assembly/bloc/entities.dart
@@ -2,7 +2,7 @@ import 'package:bloc/entity/entity.dart';
 import 'package:bloc/entity/facade.dart';
 import 'package:estd/ioc/service_locator.dart';
 import 'package:estd/logger.dart';
-import 'package:flutter_gateway/entity/repository.dart';
+import 'package:flutter_framework/domain/entity/repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EntitiesBlocFactory {
