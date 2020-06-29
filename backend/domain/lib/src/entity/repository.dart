5736544428f5248d2aa9fe5644e8898afdd5f7c6
Andrey Ilyunin <orkay255@gmail.com>
2020-06-29 23:10:41 +0300

    Add support for multiple locales as well as dynamic locale update handling

--- backend/domain/lib/src/entity/repository.dart
+++ backend/domain/lib/src/entity/repository.dart
@@ -4,6 +4,6 @@ abstract class EntityRepository {
 
   static const int noLimit = -1;
 
-  Future<List<Entity>> getAll({int limit = 200});
+  Future<List<Entity>> getAll(String localeCode, {int limit = 200});
 
 }
